e9df0c5a753b483196839bf53b317b73d5437fba
Ryan Geary <rgeary@lyft.com>
2022-11-10 20:38:35 -0500

    copy app names from theryangeary

--- core/application_matches.py
+++ core/application_matches.py
@@ -5,6 +5,16 @@ mod = Module()
 
 apps = mod.apps
 
+apps.alacrity = """
+os: mac
+and app.name: Alacritty
+"""
+
+apps.meet = """
+os: mac
+and app.name: Google Meet
+"""
+
 # apple specific apps
 apps.datagrip = """
 os: mac
